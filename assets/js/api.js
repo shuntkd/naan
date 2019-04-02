08adcb5d64eb740fe9cdf91978003d11254eb876
香田 俊太朗 <31662724+shuntkd@users.noreply.github.com>
2019-04-02 20:18:19 +0900

    プルダウン追加　エリアコード途中

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,62 +1,88 @@
+
+
+/** Sエリア情報を配列へ*/
+function s_gword( result ) {
+    for(var i in result.garea_small ){
+        availableTags[result.garea_small[i].areaname_s]=result.garea_small[i].areacode_s;
+        list.push(esult.garea_small[i].areaname_s);
+    };
+};
+
+/** Mエリア情報を配列へ追加*/
+function m_gword( result ) {
+    for(var i in result.garea_middle ){
+        availableTags[result.garea_middle[i].areaname_m]=result.garea_middle[i].areacode_m;
+        list.push(result.garea_middle[i].areaname_m);
+    };
+};
+
+
+function　gword() {
+
+    availableTags = [];
+    list = [];
+
+    /**Sエリア情報取得 */
+    var area = new XMLHttpRequest();
+    var url = "https://api.gnavi.co.jp/master/GAreaSmallSearchAPI/v3/?keyid=2fec3133982fe68d86c709c6594f120c";
+    area.open("get",url);
+    area.responseType = 'json';
+    area.send();
+    area.onload =function( data ) {       
+            s_gword(data.target.response);
+        };
+    /**Mエリア情報取得 */
+    var url = "https://api.gnavi.co.jp/master/GAreaMiddleSearchAPI/v3/?keyid=2fec3133982fe68d86c709c6594f120c";
+    area.open("get",url);
+    area.responseType = 'json';
+    area.send();
+    area.onload =function( data ) {       
+            m_gword(data.target.response);
+        };
+ 
+
+    $( "#freeword" ).autocomplete({
+      /**プルダウンリスト */
+      source:list
+    });
+};
+
 function showResult( result ) {
+    var thumbnail =   $(".thumbnail");
     for ( var i in result.rest ) { 
-      $("#thumbnail").append('<li><a href=""><div class="shopImg"><img src='+result.rest[i].image_url.shop_image1+' alt="image"></div><div class="shopName"><p>'+result.rest[i].name+'<br>'+result.rest[i].access.station+'</p></div></a></li>');  
+    thumbnail.append('<li><a href=""><div class="shopImg"><img src='+result.rest[i].image_url.shop_image1+' alt="image"></div><div class="shopName"><p>'+result.rest[i].name+'<br>'+result.rest[i].access.station+'</p></div></a></li>');  
     };
   };
 
-function freewordsearch(){
-    
-    $("#submit").click(function () {
-        $("#thumbnail").empty();
-        $("#searchWord").empty();
+
+function freewordSearch(){
+        var thumbnail = $(".thumbnail");
+        thumbnail.empty();
         var ajax = new XMLHttpRequest();
-        var freeword = "";
-        freeword=$("#freeword").val();
-        $("#searchWord").append("<li><p>"+freeword+"</p></li>");
-        var url = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=キーを入れる&freeword="+freeword;
-        ajax.open("get",url);
-        ajax.responseType = 'json';
-        ajax.send();
-        ajax.onload =function( e ) {
-            showResult(e.target.response);
-        }
-
-    });
-        
-
-};
-
-function freewordsearch2(){
-    
-        $("#thumbnail").empty();
-        $("#freeWord").empty();
-        var ajax = new XMLHttpRequest();
-
-        var freeword = "";
 
         var arg  = new Object;
         ur = location.search.substring(1).split('&');
-
         for(i=0; ur[i]; i++) {
             var k = ur[i].split('=');
             arg[k[0]] = k[1];
         }
 
-        freeword = decodeURI(arg.freeword);
+        var areaName = decodeURI(arg.freeword);
+        var areaCode = availableTags[areaName];
 
-        $("#searchWord").append("<li><p>"+freeword+"</p></li>");
-        var url = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=キーを入れる&freeword="+freeword;
+        var word=$(".searchWord");
+
+        word.append("<li><p>"+areaName+"</p></li>");
+        var url = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=2fec3133982fe68d86c709c6594f120c&areacode_m="+areaCode;
         ajax.open("get",url);
         ajax.responseType = 'json';
         ajax.send();
-        ajax.onload =function( e ) {
-            showResult(e.target.response);
+        ajax.onload =function( data ) {
+            showResult(data.target.response);
         }
-        
-
 };
 
 $(window).load(function(){
-    freewordsearch();
-    freewordsearch2();
+    gword();
+    freewordSearch();
 });
